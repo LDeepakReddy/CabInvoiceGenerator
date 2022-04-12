4d88d9094f19709a6693121ca41e5391fc85dd7a
LDeepakReddy <l.deepakreddy.com>
2022-04-13 00:45:54 +0530

    ADDED:Test case to return minimum total fare

--- src/test/java/com/blz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/blz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -4,11 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceService invoiceService = new InvoiceService();
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceService invoiceService=new InvoiceService();
-        double totalFare= invoiceService.calculateFare(2.0,5);
-        Assert.assertEquals(25,totalFare,0);
+        double totalFare = invoiceService.calculateFare(2.0, 5);
+        Assert.assertEquals(25, totalFare, 0);
+    }
 
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumTotalFare() {
+        double totalFare = invoiceService.calculateFare(0.1, 1);
+        Assert.assertEquals(5, totalFare, 0);
     }
 }
