531622ed11a69f42a2d5dbb5dc22ca5f901fff01
LDeepakReddy <l.deepakreddy.com>
2022-04-12 19:18:20 +0530

    ADDED:Welcome message to the cab invice generator program

---
+++ src/main/java/com/blz/cabinvoicegenerator/InvoiceService.java
@@ -0,0 +1,7 @@
+package com.blz.cabinvoicegenerator;
+
+public class InvoiceService {
+    public static void main(String[] args){
+        System.out.println("welcome to the cab invoice generator program");
+    }
+}
